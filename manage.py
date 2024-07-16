5d6585d6e58c8005203053d3b3f11e06e466c511
dgusfr <diegogustavo.fr@gmail.com>
2024-07-16 10:06:00 -0300

    more changes

--- manage.py
+++ manage.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
@@ -7,7 +5,6 @@ from dotenv import load_dotenv
 
 
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
     load_dotenv()
     try:
